86708b2172f283ec2082d3f516feb1f94d3a37e3
jaime-alex <jaime-alex@outlook.com>
2024-09-14 21:18:16 -0700

    add background and nav bar

---
+++ src/nav-bar/nav-bar.tsx
@@ -0,0 +1,8 @@
+import './styles.css'
+
+export default function NavBar () {
+    return <div className='nav-bar'>
+        <button className='clear'>Home</button>
+        <button className='clear'>Log In</button>
+    </div>
+}
