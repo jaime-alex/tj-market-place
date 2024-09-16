4d9784c08e577f7408df1d272f5287645dfaf00f
jaime-alex <jaime-alex@outlook.com>
2024-09-15 23:05:44 -0700

    refactors

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,10 @@ import Home from './home/home';
 import Identify from './identify/identify';
 import NavBar from './nav-bar/nav-bar'
 import {
-  createBrowserRouter,
   Route,
   BrowserRouter as Router,
-  RouterProvider,
   Routes,
 } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home></Home>,
-  },
-  {
-    path: "/identify",
-    element: <Identify></Identify>
-  }
-]);
 
 function App() {
 
