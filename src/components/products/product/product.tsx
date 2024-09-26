ebfdcf281227e866d163baae288d8e1e83648964
jaime-alex <jaime-alex@outlook.com>
2024-09-26 00:07:13 -0700

    fis issue

--- src/components/products/product/product.tsx
+++ src/components/products/product/product.tsx
@@ -4,7 +4,7 @@ import styles from "./Product.module.css"
 export default function Product(props: any) {
     return <Card className={styles.product}>
         <img className={styles.image} src="https://media.istockphoto.com/id/1300459022/photo/natural-organic-spa-cosmetic-products-set-with-eucalyptus-leaves-top-view-herbal-skincare.jpg?s=612x612&w=0&k=20&c=_xkB2_OnFqzJKVdDCeNCPeMp4jwLTsSQy2VvRloiPgk="></img>
-        <h4>props.title</h4>
+        <h4>{props.title}</h4>
         {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p> */}
         <div className={styles.price}>
             <span className={styles.symbol}>$</span>
