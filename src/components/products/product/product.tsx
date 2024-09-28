1cbaa2075c54dc3c5c9d5f94f2c83a4e7fe44e97
jaime-alex <jaime-alex@outlook.com>
2024-09-28 12:38:18 -0700

    Add image processing

--- src/components/products/product/product.tsx
+++ src/components/products/product/product.tsx
@@ -1,18 +1,29 @@
+import { useEffect, useState } from "react"
 import Card from "../../common/Card"
 import styles from "./Product.module.css"
 
 export default function Product(props: any) {
+    const [imageUrl, setImageUrl] = useState(null)
+
+    useEffect(() => {
+        if (props.images) {
+            const imagesArray: string[] = JSON.parse(props.images)
+            setImageUrl(imagesArray[0])
+        }
+    },[])
     return <div className={styles.product}>
         <Card>
-        <img className={styles.image} src="https://media.istockphoto.com/id/1300459022/photo/natural-organic-spa-cosmetic-products-set-with-eucalyptus-leaves-top-view-herbal-skincare.jpg?s=612x612&w=0&k=20&c=_xkB2_OnFqzJKVdDCeNCPeMp4jwLTsSQy2VvRloiPgk="></img>
-        <h4>{props.title}</h4>
-        {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p> */}
-        <div className={styles.price}>
-            <span className={styles.symbol}>$</span>
-            <span className={styles.value}>750</span>
-            <span className={styles.fraction}>00</span>
-        </div>
-        <button className={`yellow ${styles.button}`}>Agregar a carrito</button>
-    </Card>
+            {imageUrl 
+                ? <img className={styles.imageCover} src={imageUrl}></img> 
+                : <img className={styles.imageContain} src={"/src/assets/image_placeholder.svg"}></img> }
+            <h4>{props.title}</h4>
+            {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p> */}
+            <div className={styles.price}>
+                <span className={styles.symbol}>$</span>
+                <span className={styles.value}>750</span>
+                <span className={styles.fraction}>00</span>
+            </div>
+            <button className={`yellow ${styles.button}`}>Agregar a carrito</button>
+        </Card>
     </div>
 }
