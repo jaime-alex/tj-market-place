036731e12666c9255f5fd7f80dd67337d45d9473
jaime-alex <jaime-alex@outlook.com>
2024-10-07 19:57:51 -0700

    add more details

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,6 +1,6 @@
 const Card = (props: any) => (
     <div style={{
-            border: "1px solid gray", 
+            border: "1px solid #d6d6d6", 
             borderRadius: "5px",
             backgroundColor: "rgb(255, 255, 255)",
             padding: "10px"
--- src/components/product-details/product-details.tsx
+++ src/components/product-details/product-details.tsx
@@ -1,5 +1,7 @@
 import { useLocation } from "react-router-dom";
 import css from "./ProductDetails.module.css"
+import { useState } from "react";
+import placeHolderImage from "../../assets/image_placeholder.svg"
 
 interface Props {
     images: Array<string>,
@@ -9,14 +11,19 @@ interface Props {
 
 export default function ProductDetails() {
     const location = useLocation();
-    const { images } : Props = location.state || {};
+    const { images, title }: Props = location.state || {};
+    const [mainImage, setMainImage] = useState<string>(images?.length > 0 ? images[0] : placeHolderImage)
 
     return <div className={css.container}>
-        <div className={css.images}>
-            <div className={css.imagesCollection}></div>
-            <img className={css.mainImage} src={images[0]}></img>
+            <div className={css.images}>
+                <div className={css.imagesCollection}>
+                    {images?.map((image, i) => <img key={`product-details-${i}`} src={image} className={css.thumbnailImg}></img>)}
+                </div>
+                <img className={css.mainImage} src={mainImage}></img>
+            </div>
+            <div className={css.details}>
+                <h2>{title}</h2>
+            </div>
+            <div className={css.options}></div>
         </div>
-        <div className={css.details}></div>
-        <div className={css.options}></div>
-    </div>
 }
